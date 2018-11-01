12b19e52c45891e1d43b3e4725fba6c0e8365618
Salvador Guzman <guzmansalv@gmail.com>
2018-11-01 04:52:03 -0700

    moved sql stuff

---
+++ src/postgres/conn.go
@@ -0,0 +1,63 @@
+package postgres
+
+import (
+    "database/sql"
+    _ "github.com/lib/pq"
+    "fmt"
+    "os"
+)
+
+const (
+    defaultHost = "192.168.1.63"
+    defaultPort = "30000"
+)
+
+func connStr() string {
+    host := os.Getenv("DB_HOST")
+    port := os.Getenv("DB_PORT")
+
+    if len(host) == 0 || len(port) == 0 {
+        return fmt.Sprintf("user=postgres dbname=youtube host=%s port=%s sslmode=disable", defaultHost, defaultPort)
+    } else {
+        return fmt.Sprintf("user=postgres dbname=youtube host=%s port=%s sslmode=disable", host, port)
+    }
+}
+
+func connection() *sql.DB {
+    db, err := sql.Open("postgres", connStr())
+    if err != nil {
+        panic(err)
+    }
+
+    return db
+}
+
+func Channels() []string {
+    sqlStr := "select serial from youtube.entities.channels LIMIT 50"
+    db := connection()
+    defer func() {
+        err := db.Close()
+        if err != nil {
+            panic(err)
+        }
+    }()
+
+    row, err := db.Query(sqlStr)
+    if err != nil {
+        panic(err)
+    }
+
+    serials := make([]string, 0)
+    for row.Next() {
+        var serial string
+
+        err = row.Scan(&serial)
+        if err != nil {
+            panic(err)
+        }
+
+        serials = append(serials, serial)
+    }
+
+    return serials
+}
